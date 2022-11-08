1968ccb0ac8280dc0a087e44617f2033e6a53233
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-11-09 03:27:51 +0600

    find operation added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -13,8 +13,31 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3s0wivp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+  try{
+    const serviceCollection = client.db('shutterPhotography').collection('services')
 
+    // multiple data
+    app.get('/services', async(req, res)=>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
+    // single data
+    app.get('/service/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    })
+  }
+  finally{
+
+  }
+}
+run().catch(err => console.error(err));
 
 
 
