68d2df74940a96d5a9f7cecb0419b0698ea5b110
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-11-10 22:29:08 +0600

    all done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -13,13 +13,14 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3s0wivp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-  try{
-    const serviceCollection = client.db('shutterPhotography').collection('services');
-    const reviewCollection = client.db('shutterPhotography').collection('userReview');
+async function run() {
+  try {
+    const serviceCollection = client.db("shutterPhotography").collection("services");
+    const reviewCollection = client.db("shutterPhotography").collection("userReview");
+    const reviewUserCollection = client.db("shutterPhotography").collection("review");
 
     // multiple data servicecollection
-    app.get('/services', async(req, res)=>{
+    app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
@@ -27,50 +28,57 @@ async function run(){
     });
 
     // single data serviceCollection
-    app.get('/service/:id', async(req, res) =>{
+    app.get("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const result = await serviceCollection.findOne(query);
       res.send(result);
     });
 
     // review query with email
-    app.get('/review', async(req, res)=>{
+    app.get("/review", async (req, res) => {
       let query = {};
-      if(req.query.email){
-        query={
-          userEmail: req.query.email
-        }
+      if (req.query.email) {
+        query = {
+          userEmail: req.query.email,
+        };
       }
       const reviewData = reviewCollection.find(query);
       const queryReview = await reviewData.toArray();
       res.send(queryReview);
-    })
-    // review data query by service_id
-    app.get('/review', async(req, res)=>{
-      let query = {};
-      if(req.query.service_id){
-        query={
-          serviceId: req.query.service_id
-        }
-      }
-      const reviewResult = reviewCollection.find(query);
-      const reviews =  await reviewResult.toArray();
-      res.send(reviews);
     });
-    app.post('/review', async(req, res)=>{
-      const reviews = req.body;
-      const result = await reviewCollection.insertOne(reviews);
+    // review data query by service_id
+    app.get("/reviewbyuser", async (req, res) => {
+      const serviceId = req.query.serviceId;
+      const query = { serviceId: serviceId };
+      const reviewByUser = await reviewCollection.find(query).toArray();
+      res.send(reviewByUser);
+    });
+
+    app.post("/review", async (req, res) => {
+      const reviewDetails = req.body;
+      const result = await reviewCollection.insertOne(reviewDetails);
       res.send(result);
     });
-  }
-  finally{
+    // review update
+    app.get('/updatereview/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
+    })
+    // delete review
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
 
   }
 }
-run().catch(err => console.error(err));
-
-
+run().catch((err) => console.error(err));
 
 // testing
 app.get("/", (req, res) => {
